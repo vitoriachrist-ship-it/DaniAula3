845a6ac1c2001183ed8312ef21455f8e23454612
vitoriachrist-ship-it <vitoria.christ@aluno.fmpsc.edu.br>
2026-03-29 20:01:03 -0300

    Add files via upload

---
+++ Aula3.Ex1.py
@@ -0,0 +1,15 @@
+primeirovalor = int(input("Qual primeiro valor?"))
+segundovalor = int(input("Qual segundo valor?"))
+
+soma = (primeirovalor + segundovalor)
+subtração = (primeirovalor - segundovalor)
+multiplicação = (primeirovalor * segundovalor)
+divisão = (primeirovalor / segundovalor)
+mod = (primeirovalor % segundovalor)
+
+print(soma)
+print(subtração)
+print(multiplicação)
+print(divisão)
+print(mod)
+
